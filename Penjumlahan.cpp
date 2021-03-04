825cf4e44808e698bb77d5ec2251cfafde934078
sucikhrr <52316304+sucikhrr@users.noreply.github.com>
2021-03-04 10:28:05 +0700

    menambahkan perkalian

--- Penjumlahan.cpp
+++ Penjumlahan.cpp
@@ -1,16 +1,23 @@
 #include <conio.h>
 #include <iostream>
 using namespace std;
-
+int a = 4, b=2 ;
 int pengurangan()
 {
-int a = 4, b=2, jumlah ;
+int jumlah ;
 jumlah = a - b ;
 return jumlah ;
+}
+
+int perkalian()
+{ int kali; 
+ kali = a* b;
+ return kali;
 }
 
 int main ()
 {
 cout<<"Pengurangan a dan b = "<<pengurangan();
+cout<<"Perkalian a dan b = "<<perkalian();
 return 0;
 }
