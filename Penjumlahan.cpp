9135e543e9faef68cafe843d6b406215b194460c
sucikhrr <52316304+sucikhrr@users.noreply.github.com>
2021-03-04 10:24:55 +0700

    penjumlahan a dan b

---
+++ Penjumlahan.cpp
@@ -0,0 +1,16 @@
+#include <conio.h>
+#include <iostream>
+using namespace std;
+
+int penjumlahan()
+{
+int a = 2, b=4, jumlah ;
+jumlah = a +b ;
+return jumlah ;
+}
+
+int main ()
+{
+cout<<"Penjumlahan a dan b = "<<penjumlahan();
+return 0;
+}
